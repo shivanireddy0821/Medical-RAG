3fd3016ab2d1e7d03d76c751c902eba6e0d1afb0
Shivani Annu <shivanireddy0821@gmail.com>
2025-11-01 00:04:58 +0530

    folder added

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+# Creating files
+touch src/_init_ py
+touch src/helper. py
+touch src/prompt. py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt 
+echo "files created"
