6ec1d45312e430b0f7af6b804b28a01a841b3d43
Shivani Annu <shivanireddy0821@gmail.com>
2025-11-01 18:08:36 +0530

    requirements added

--- template.sh
+++ template.sh
@@ -1,9 +1,9 @@
 mkdir -p src
 mkdir -p research
 # Creating files
-touch src/_init_ py
-touch src/helper. py
-touch src/prompt. py
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
 touch .env
 touch setup.py
 touch app.py
